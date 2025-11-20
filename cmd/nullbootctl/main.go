7564c6c46ef6a653d6b7bf82da69eef629027755
Michael Raymond <michael.raymond@canonical.com>
2025-11-20 16:48:47 -0500

    feat: CLI update to support fallback mechanism
    This enables the kernel fallback mechanism and amendment method via
    `nullbootctl`. The addition of these two CLI flags enables a default
    kernel fallback mechanism on new kernel installations. Default behavior
    is to implement the fallback mechanism, and a successful boot onto that
    kernel will amend the boot loader if -confirm-kernel-boot is used.
    -trust-kernel-installs essentially performs the previous nullboot
    behavior.

--- cmd/nullbootctl/main.go
+++ cmd/nullbootctl/main.go
@@ -9,9 +9,11 @@ import "flag"
 import "log"
 import "os"
 
+var confirmKernelBoot = flag.Bool("confirm-kernel-boot", false, "Ensures BootCurrent refers to the latest kernel; if BootCurrent is not yet the first entry of BootOrder, amends the boot loader; otherwise, exits for safety. Intended for use on start-up after kernel installations")
+var trustKernelInstalls = flag.Bool("trust-kernel-installs", false, "Automatically adds a kernel to the boot loader and deletes obsolete boot entries; otherwise, boot loader is not updated on kernel installations until the latest kernel successfully boots via BootNext")
 var noTPM = flag.Bool("no-tpm", false, "Do not do any resealing with the TPM")
-var noEfivars = flag.Bool("no-efivars", false, "Do not use or update the EFI variables")
-var outputJSON = flag.String("output-json", "", "JSON file to write (also disables writing real EFI variables)")
+var noEfivars = flag.Bool("no-efivars", false, "Do not use or update the EFI variables. Disables kernel fallback mechanism")
+var outputJSON = flag.String("output-json", "", "JSON file to write. Disables writing real EFI variables and enablement of the kernel fallback mechanism")
 
 func main() {
 	var assets *efibootmgr.TrustedAssets
@@ -25,7 +27,14 @@ func main() {
 		vendor          = "ubuntu"
 	)
 
-	// FIXME: Let's actually add some arg parsing and stuff?
+	usingRealEFIVars := *outputJSON == "" && !*noEfivars
+	if *confirmKernelBoot {
+		if !usingRealEFIVars {
+			log.Println("One of the selected flags disables reading of system EFI variables; cannot confirm the status of BootCurrent if system EFI variables are not used")
+			os.Exit(1)
+		}
+	}
+
 	if !*noTPM {
 		assets, err = efibootmgr.ReadTrustedAssets()
 		if err != nil {
@@ -102,34 +111,69 @@ func main() {
 		os.Exit(1)
 	}
 
-	if err = km.CommitToBootLoader(); err != nil {
-		log.Print(err)
-		os.Exit(1)
-	}
-	// Cleanup old entries
-	if err = km.RemoveObsoleteKernels(); err != nil {
-		log.Print(err)
-		os.Exit(1)
-	}
-	if err = km.CommitToBootLoader(); err != nil {
-		log.Print(err)
-		os.Exit(1)
-	}
-
-	if assets != nil {
-		assets.RemoveObsolete()
-		if err := assets.Save(); err != nil {
-			log.Println("cannot update list of trusted boot assets:", err)
-			os.Exit(1)
-		}
-
-		// Final reseal to remove obsolete assets from profile
-		if err := efibootmgr.ResealKey(assets, km, esp, shimSourceDir, vendor); err != nil {
-			log.Println("final reseal failed:", err)
-			os.Exit(1)
+	// Determine if the fallback mechanism is required
+	setKernelFallback := false
+	if !(*trustKernelInstalls) {
+		if usingRealEFIVars {
+			// Only set fallback if the latest kernel is not booted
+			res, err := km.IsCurrentBootLatest()
+			if err != nil {
+				log.Printf("Unable to determine if the latest kernel is BootCurrent: %v", err)
+				os.Exit(1)
+			}
+			setKernelFallback = !res
 		}
 	}
 
+	if *confirmKernelBoot {
+		// If current boot is not latest, a fallback may have occurred
+		isCurrentBootLatest, err := km.IsCurrentBootLatest()
+		if err != nil {
+			log.Printf("Unable to determine if a kernel fallback occurred: %v", err)
+			os.Exit(1)
+		}
+		if !isCurrentBootLatest {
+			// Do not ammend the boot loader if a fallback occurred
+			log.Printf("BootCurrent is not the latest kernel; fallback may have occurred")
+			os.Exit(1)
+		}
+	}
+
+	if setKernelFallback {
+		if err := km.SetKernelFallback(); err != nil {
+			log.Printf("Unable to set kernel fallback for new kernel: %v", err)
+			os.Exit(1)
+		}
+		log.Println("Set kernel fallback mechanism for newly installed kernel")
+	} else {
+		if err = km.CommitToBootLoader(); err != nil {
+			log.Print(err)
+			os.Exit(1)
+		}
+		// Cleanup old entries
+		if err = km.RemoveObsoleteKernels(); err != nil {
+			log.Print(err)
+			os.Exit(1)
+		}
+		if err = km.CommitToBootLoader(); err != nil {
+			log.Print(err)
+			os.Exit(1)
+		}
+
+		if assets != nil {
+			assets.RemoveObsolete()
+			if err := assets.Save(); err != nil {
+				log.Println("cannot update list of trusted boot assets:", err)
+				os.Exit(1)
+			}
+
+			// Final reseal to remove obsolete assets from profile
+			if err := efibootmgr.ResealKey(assets, km, esp, shimSourceDir, vendor); err != nil {
+				log.Println("final reseal failed:", err)
+				os.Exit(1)
+			}
+		}
+	}
 	if jsonEfivars, ok := efivars.(*efibootmgr.MockEFIVariables); ok {
 		json, err := jsonEfivars.JSON()
 		if err != nil {
